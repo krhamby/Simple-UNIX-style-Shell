f28225920d6e7cd11d8b78c0139707e88a61f5f0
krhamby <hambykr19@gcc.edu>
2023-02-09 17:07:35 -0500

    Working shell

--- shell.c
+++ shell.c
@@ -1,7 +1,7 @@
 /*
-  Author:  He (David) Zhang
+  Author:  Kevin Hamby
   Course:  COMP 340, Operating Systems
-  Date:    10 September 2021
+  Date:    13 February 2023
   Description:   This file implements the
                  Shell program
   Compile with:  gcc -o shell shell.c
@@ -21,42 +21,28 @@
 char* get_current_dir();
 
 int shell_change_dir(char *dir_path) {
-  // use the chdir() system call to change the current directory
   return chdir(dir_path);
 }
 
-
 int shell_file_exists(char *file_path) {
-  // use the stat() system call to check if a file exists
   struct stat buffer;
-  return (stat(file_path, &buffer) == 0);
+  return stat(file_path, &buffer);
 }
 
-// NOTE: I changed the file_path to pass-by-reference
 int shell_find_file(char *file_name, char *file_path, char file_path_size) {
   // traverse the PATH environment variable to find the absolute path of a file/command
-  char* cwd = get_current_dir();
-  char* cwd_copy = strdup(cwd);
-  strcat(cwd_copy, "/");
-  strcat(cwd_copy, file_name);
-  if (shell_file_exists(cwd_copy)) {
-    strcpy(file_path, cwd_copy);
-    return 0;
-  } else {
-    char *path = getenv("PATH");
-    char *path_copy = strdup(path);
-    char *path_token = strsep(&path_copy, ":");
-    char tmp[1024] = {0}; 
-    while (path_token != NULL) {
-      char *file_path_copy = strdup(path_token);
-      strcat(file_path_copy, "/");
-      strcat(file_path_copy, file_name);
-      if (shell_file_exists(file_path_copy)) {
-        strcpy(file_path, file_path_copy);
-        return 0;
-      }
-      path_token = strsep(&path_copy, ":");
+  char *path = getenv("PATH");
+  char *path_copy = strdup(path);
+  char *path_token = strsep(&path_copy, ":");
+  while (path_token != NULL) {
+    char *file_path_copy = strdup(path_token);
+    strcat(file_path_copy, "/");
+    strcat(file_path_copy, file_name);
+    if (shell_file_exists(file_path_copy) == 0) {
+      strcpy(file_path, file_path_copy);
+      return 0;
     }
+    path_token = strsep(&path_copy, ":");
   }
   return -1;
 }
@@ -66,20 +52,18 @@ int shell_execute(char *file_path, char **argv) {
   // execute the file with the command line arguments
   // use the fork() and exec() system call 
   pid_t pid;
-  int status;
   pid = fork();
   if (pid == 0) {
     execv(file_path, argv);
     perror("execv");
-    printf("Error: execv failed\n");
     exit(0);
   } else if (pid < 0) {
-    printf("Error: fork failed\n");
+    perror("fork");
     exit(0);
   } else {
-    waitpid(pid, &status, 0);
+    wait(NULL);
+    return 0;
   }
-  return 0;
 }
 
 // thank you internet
@@ -93,15 +77,103 @@ char* remove_whitespace(char *str) {
   return str;
 }
 
-char* get_current_dir() {
-  char *cwd = (char*) malloc(1000);
-  getcwd(cwd, 1000);
-  return cwd;
-}
-
 int main (int argc, char *argv[]) {
-   //run the shell
-   
+  //run the shell
+  char *command;
+  printf("kevin@shell:~%s$ ", getcwd(NULL, 0));
+  while (1) {
+    char* input = (char*) malloc(1024);
+    fgets(input, 1024, stdin);
+
+    char* args[10] = {NULL};
+    char *tmp = strsep(&input, " ");
+    int i = 0;
+    while (tmp != NULL) {
+      tmp = remove_whitespace(tmp);
+      args[i] = tmp;
+      tmp = strsep(&input, " ");
+      i++;
+    }
+
+    command = args[0];
+    char* file_path = (char*) malloc(1024);
+
+    if (strcmp(command, "exit") == 0) {
+      return 0;
+    } else if (strcmp(command, "cd") == 0) {
+      shell_change_dir(args[1]);
+    } else if (shell_file_exists(command) == 0) {
+      shell_execute(command, args);
+    } else if (shell_find_file(command, file_path, 0) == 0) {
+      file_path = remove_whitespace(file_path);
+      shell_execute(file_path, args);
+    } else {
+      printf("Error: command '%s' does not exists\n", command);
+    }
+
+    printf("kevin@shell:~%s$ ", getcwd(NULL, 0));
+
+    free(input);
+    free(file_path);
+
+
+
+
+
+
+
+  //   char *first_arg = strsep(&command, " ");
+  //   first_arg = remove_whitespace(first_arg);
+  //   if (strcmp(first_arg, "exit") == 0) {
+  //     exit = 1;
+  //   } else if (strcmp(first_arg, "cd") == 0) {
+  //     char *dir_path = strsep(&command, " ");
+  //     if (dir_path != NULL) {
+  //       dir_path = remove_whitespace(dir_path);
+  //     } else {
+  //       dir_path = "";
+  //     }
+  //     shell_change_dir(dir_path);
+  //   } else {
+  //     // to be written to in shell_find_file
+  //     char *file_path = (char*) malloc(1024);
+
+  //     char *tmp = strsep(&command, " ");
+  //     int i = 1;
+  //     char *args[] = {first_arg};
+  //     printf("first_arg: %s\n", first_arg);
+  //     while (tmp != NULL) {
+  //       args[i] = tmp;
+  //       tmp = strsep(&command, " ");
+  //       i++;
+  //     }
+
+  //     args[i] = NULL;
+
+  //     for (int j = 0; j < i; j++) {
+  //       printf("args: %s\n", args[j]);
+  //     }
+      
+  //     if (shell_find_file(first_arg, file_path, 0) == 0) {
+  //       printf("file_path: %s\n", file_path);
+  //       shell_execute(file_path, args);
+  //     } else {
+  //       printf("Error: command not found\n");
+  //     }
+  //   }
+  //   if (!exit) {
+  //     printf("kevin@shell:~%s$ ", get_current_dir());
+  //   }
+
+  //   // free stuff
+  //   free(command);
+  // }
+  // // free again
+  // free(command);
+
+  // return 0;
+
+
    /*
    while (!exit)
    {
@@ -123,54 +195,5 @@ int main (int argc, char *argv[]) {
 			report an error message
     }
    */
-
-  int exit = 0;
-  char *command;
-  printf("kevin@UnixShell:~%s$ ", get_current_dir());
-  while (!exit) {
-    command = malloc(1000);
-    fgets(command, 1000, stdin);
-    char *first_arg = strsep(&command, " ");
-    first_arg = remove_whitespace(first_arg);
-    if (strcmp(first_arg, "exit") == 0) {
-      exit = 1;
-    } else if (strcmp(first_arg, "cd") == 0) {
-      char *dir_path = strsep(&command, " ");
-      if (dir_path != NULL) {
-        dir_path = remove_whitespace(dir_path);
-      } else {
-        dir_path = "";
-      }
-      shell_change_dir(dir_path);
-    } else {
-      // to be written to in shell_find_file
-      char *file_path = (char*) malloc(1000);
-
-      char *tmp = strsep(&command, " ");
-      int i = 1;
-      char *args[] = {first_arg};
-      while (tmp != NULL) {
-        args[i] = tmp;
-        tmp = strsep(&command, " ");
-      }
-
-      // // error handling for file_path -> 
-      // if (file_path != NULL) {
-      //   file_path = remove_whitespace(file_path);
-      // } else {
-      //   file_path = "";
-      // }
-      if (shell_find_file(first_arg, file_path, 100) == 0) {
-        shell_execute(file_path, &command);
-      } else {
-        printf("Error: command not found\n");
-      }
-    }
-    if (!exit) {
-      printf("kevin@UnixShell:~%s$ ", get_current_dir());
-      free(command);
-    } else {
-      free(command);
-    }
-  }
+}
 }
