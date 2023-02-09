b441f9737dfecdb4dcd0cbc9338e0c0bcbb3fe1c
krhamby <hambykr19@gcc.edu>
2023-02-09 13:25:21 -0500

    Working code; need to ask Dr. Zhang some questions

--- shell.c
+++ shell.c
@@ -3,28 +3,78 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <ctype.h>
 
 
 int shell_change_dir(char *dir_path) {
   // use the chdir() system call to change the current directory
+  return chdir(dir_path);
 }
 
 
 int shell_file_exists(char *file_path) {
   // use the stat() system call to check if a file exists
+  struct stat buffer;
+  return (stat(file_path, &buffer) == 0);
 }
 
-
-int shell_find_file(char *file_name, char *file_path, char file_path_size) {
+// NOTE: I changed the file_path to pass-by-reference
+int shell_find_file(char *file_name, char **file_path, char file_path_size) {
   // traverse the PATH environment variable to find the absolute path of a file/command
+  char *path = getenv("PATH");
+  char *path_copy = strdup(path);
+  char *path_token = strsep(&path_copy, ":");
+  while (path_token != NULL) {
+    char *file_path_copy = strdup(path_token);
+    strcat(file_path_copy, "/");
+    strcat(file_path_copy, file_name);
+    if (shell_file_exists(file_path_copy)) {
+      // strcpy(file_path, file_path_copy);
+      *file_path = file_path_copy;
+      return 0;
+    }
+    path_token = strsep(&path_copy, ":");
+  }
+  return -1;
 }
 
 
 int shell_execute(char *file_path, char **argv) {
   // execute the file with the command line arguments
   // use the fork() and exec() system call 
+  pid_t pid;
+  int status;
+  pid = fork();
+  if (pid == 0) {
+    execv(file_path, argv);
+    perror("execv");
+    printf("Error: execv failed\n");
+    exit(0);
+  } else if (pid < 0) {
+    printf("Error: fork failed\n");
+    exit(0);
+  } else {
+    waitpid(pid, &status, 0);
+  }
+  return 0;
 }
 
+// thank you internet
+char* remove_whitespace(char *str) {
+  char *end;
+  while (isspace((unsigned char)*str)) str++;
+  if (*str == 0) return str;
+  end = str + strlen(str) - 1;
+  while (end > str && isspace((unsigned char)*end)) end--;
+  end[1] = '\0';
+  return str;
+}
+
+char* get_current_dir() {
+  char *cwd = (char*) malloc(257);
+  getcwd(cwd, 257);
+  return cwd;
+}
 
 int main (int argc, char *argv[]) {
    //run the shell
@@ -50,4 +100,47 @@ int main (int argc, char *argv[]) {
 			report an error message
     }
    */
+
+  int exit = 0;
+  char *command;
+  printf("username@hostname:~%s$ ", get_current_dir());
+  while (!exit) {
+    command = malloc(257);
+    fgets(command, 100, stdin);
+    char *first_arg = strsep(&command, " ");
+    first_arg = remove_whitespace(first_arg);
+    if (strcmp(first_arg, "exit") == 0) {
+      exit = 1;
+    } else if (strcmp(first_arg, "cd") == 0) {
+      char *dir_path = strsep(&command, " ");
+      if (dir_path != NULL) {
+        dir_path = remove_whitespace(dir_path);
+      } else {
+        dir_path = "";
+      }
+      shell_change_dir(dir_path);
+    } else {
+      char *file_path = (char*) malloc(100);
+      file_path = strsep(&command, " ");
+
+      // error handling for file_path
+      if (file_path != NULL) {
+        file_path = remove_whitespace(file_path);
+      } else {
+        file_path = "";
+      }
+
+      if (shell_find_file(first_arg, &file_path, 100) == 0) {
+        shell_execute(file_path, &command);
+      } else {
+        printf("Error: command not found\n");
+      }
+    }
+    if (!exit) {
+      printf("username@hostname:~%s$ ", get_current_dir());
+      free(command);
+    } else {
+      free(command);
+    }
+  }
 }
